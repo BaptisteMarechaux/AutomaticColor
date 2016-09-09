082086de3d783c4ec07303d5e55fd3268aff7cd9
Baptiste Maréchaux <baptiste.marechaux@gmail.com>
2016-09-09 12:18:42 +0200

    color compute script (useless for now)

---
+++ AutomaticColorization/Assets/ColorCompute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorCompute {
+
+	public static bool isSimilarToNeighbour(Color[] colors, int index, int width, int height)
+    {
+
+        return false;
+    }
+}
